e4258b5b35b5b27cd8b69217a1606e5d8b496504
Edaurdo Sempere <esempere@sistemastg.com>
2020-11-01 07:32:51 -0800

    contando con unicode

--- StringNumbersMath/src/ContandoCarateresDuplicados.java
+++ StringNumbersMath/src/ContandoCarateresDuplicados.java
@@ -5,8 +5,11 @@ import java.util.stream.Collectors;
 public class ContandoCarateresDuplicados {
     
     public static void main(String[] args) {
-        System.out.println(contar("sempere"));
-        System.out.println(contarJava8("sempere"));
+       
+        System.out.println(String.valueOf(Character.toChars(128149)).length());
+    //    System.out.println(String.valueOf(Character.toChars("a".codePointAt(0)+2)));
+    //     System.out.println(contar("sempere"));
+    //     System.out.println(contarJava8("sempere"));
         
 
     }
@@ -27,15 +30,49 @@ public class ContandoCarateresDuplicados {
         return result;
         
     }
+
     
+    // Utilizando Unicode queda de la siuiente manera
+     
+
+    public static Map<String, Integer> contarUnicode(String str){
+
+        
+        Map<String, Integer> result = new HashMap<>(); 
+        for (int i = 0; i < str.length(); i++) {
+            int cp = str.codePointAt(i);
+            String ch = String.valueOf(Character.toChars(cp));
+            if (Character.charCount(cp)==2) { 
+                // En caso de que el unicode sea de los carateres 
+                // que en su representación como string sea de 
+                // longitud dos a vanzamos un lugar en mas en el for            
+                i++;
+            }
+            result.compute(ch, (key,value) -> (value == null) ? 1 : ++value);
+
+            
+        }
+
+        return result;
+    }
+
+
 
     //Esta es con Java 8
     public static Map<Character, Long> contarJava8(String str) {
 
 
-        return str.chars().mapToObj(x -> (char)x).collect(Collectors.groupingBy( c -> c, Collectors.counting()));
+        return str.chars().mapToObj(x -> (char)x)
+                          .collect(Collectors.groupingBy( c -> c, Collectors.counting()));
     }
 
+    //Esta es unicode con java 8
+    public static Map<String, Long> contarUnicodeJava8(String str) {
+
+        return str.codePoints().mapToObj(x -> String.valueOf(Character.toChars(x)))
+                  .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        
+    }
 
 
 
